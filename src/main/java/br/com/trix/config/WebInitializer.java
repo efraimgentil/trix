f5807808af53251fdf5795c571575594244b8f30
efraimgentil <efraim.gentil@gmail.com>
2016-02-18 12:59:41 -0300

    Configurações iniciais

---
+++ src/main/java/br/com/trix/config/WebInitializer.java
@@ -0,0 +1,24 @@
+package br.com.trix.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer  {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class<?>[]{ SpringWebConfig.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+	
+}
